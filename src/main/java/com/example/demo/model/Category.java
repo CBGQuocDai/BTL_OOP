c8301e0ac40e35fc3e205f07fb00709be158ab32
CBGQuocDai <quocdaicbg001@gmail.com>
2024-08-29 22:41:18 +0700

    set up

---
+++ src/main/java/com/example/demo/model/Category.java
@@ -0,0 +1,46 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Categories")
+public class Category {
+    @Id
+    @Column(name="categoryID")
+    private String categoryId;
+    private String categoryName;
+    private String description;
+    public Category(){}
+    public Category(String categoryId, String categoryName, String description) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
