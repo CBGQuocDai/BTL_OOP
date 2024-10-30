15e6bee74b75c92be042a98ee03e7fe51b62d4d2
ndh0101 <duyhoang.edu.green@gmail.com>
2024-10-30 18:09:57 +0700

    Add Report functions

---
+++ src/main/java/com/example/demo/Controller/Admin/ReportControllerA.java
@@ -0,0 +1,32 @@
+package com.example.demo.Controller.Admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.DAO.ReportDAO;
+
+@Controller
+public class ReportControllerA {
+    @Autowired
+    ReportDAO reportDAO;
+
+    @RequestMapping("/admin_report")
+    public String Report(Model model){
+        model.addAttribute("reports" , reportDAO.getAllReport());
+        return "admin/Report";
+    }
+
+    @RequestMapping("/delete-report")
+    public String deleteUser(Model model , @RequestParam("id") String reportId) throws Exception {
+        reportDAO.deleteReportById(reportId);
+        return "redirect:/admin_report";
+    }
+
+    @RequestMapping("/report-view/{id}")
+    public String ReportView(Model Model){
+        return "admin/report-view";
+    }
+}
--- src/main/java/com/example/demo/DAO/ReportDAO.java
+++ src/main/java/com/example/demo/DAO/ReportDAO.java
@@ -1,16 +1,37 @@
 package com.example.demo.DAO;
 
-import com.example.demo.Model.Report;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Component;
+
+import com.example.demo.Model.Report;
+
+@Component
 public class ReportDAO {
     private String jdbcURL = "jdbc:mysql://mysql-4bc7aa-quocdaicbg001-d224.c.aivencloud.com:16253/defaultdb";
     private String jdbcUsername = "avnadmin";
     private String jdbcPassword = "AVNS_jfijrHh9AlwIpwNz30Z";
+
+    private static final String GET_ALL_REPORT= "SELECT * FROM report";
+    private final static String ADD_REPORT ="INSERT INTO report(reportId,postId,commentId,reason,content,time) VALUES(?,?,?,?,?,NOW())";
+    private static final String GET_REPORT_BY_ID= "SELECT * FROM report WHERE reportId = ?";
+    private final String DELETE_REPORT = "DELETE FROM report WHERE reportId = ?";
+
+    public void deleteReportById (String id) throws SQLException {
+        Connection connection = getConnection();
+        PreparedStatement ps = connection.prepareStatement(DELETE_REPORT);
+
+        ps.setString(1 , id);
+
+        ps.executeUpdate();
+    }
 
     public ReportDAO(){}
     protected Connection getConnection() {
@@ -24,7 +45,30 @@ public class ReportDAO {
         return connection;
     }
 
-    private final static String ADD_REPORT ="INSERT INTO report(reportId,postId,commentId,reason,content,time) VALUES(?,?,?,?,?,NOW())";
+    public List<Report> getAllReport () {
+        List<Report> reports = new ArrayList<>();
+        try {
+            Connection connection = getConnection();
+            PreparedStatement ps = connection.prepareStatement(GET_ALL_REPORT);
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                Report report = new Report();
+                report.setReportId(rs.getInt("reportId"));
+                report.setPostId(rs.getInt("postId"));
+                report.setCommentId(rs.getInt("commentId"));
+                report.setContent(rs.getString("content"));
+                report.setReason(rs.getString("reason"));
+                report.setTime(Timestamp.valueOf(rs.getString("time")));
+                reports.add(report);
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return reports;
+    }
+
     public void addReport(Report r) throws SQLException {
         Connection connection= getConnection();
         PreparedStatement ps = connection.prepareStatement(ADD_REPORT);
@@ -35,4 +79,26 @@ public class ReportDAO {
         ps.setString(5,r.getContent());
         ps.execute();
     }
+
+    public Report selectReportById(int id){
+        Report report= new Report();
+        try {
+            Connection connection = getConnection();
+            PreparedStatement ps = connection.prepareStatement(GET_REPORT_BY_ID);
+            ps.setString(1,String.valueOf(id));
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                report.setReportId(rs.getInt("reportId"));
+                report.setPostId(rs.getInt("postId"));
+                report.setCommentId(rs.getInt("commentId"));
+                report.setContent(rs.getString("content"));
+                report.setReason(rs.getString("reason"));
+                report.setTime(Timestamp.valueOf(rs.getString("time")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return report;
+    }
+
 }
