a600437f04d5f9575b3592954c669a4c8e2da32f
CBGQuocDai <quocdaicbg001@gmail.com>
2024-11-06 19:22:28 +0700

    fix bug

--- src/main/java/com/example/demo/Controller/User/MyProfileController.java
+++ src/main/java/com/example/demo/Controller/User/MyProfileController.java
@@ -1,103 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-//package com.example.demo.Controller.User;
-//
-//import com.example.demo.DAO.PostDAO;
-//import com.example.demo.DAO.UserDAO;
-//import com.example.demo.Model.Post;
-//import com.example.demo.Model.User;
-//import java.sql.SQLException;
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-///**
-// *
-// * @author Admin
-// */
-//@Controller
-//@RequestMapping("/My_Profile")
-//public class MyProfileController {
-//
-//    @Autowired
-//    private static UserDAO userDAO;
-//    @Autowired
-//    private static PostDAO postDAO;
-//
-//    @GetMapping("/userInfo")
-//    public String userInfo(Model model) throws SQLException {
-//        // Giả sử userId = 2 cho ví dụ
-//        User user = userDAO.getUserByUserId(2);
-//        model.addAttribute("user", user);
-//        return "userInfo";
-//    }
-//
-//    @PostMapping("/userInfo")
-//    public String updateUserInfo(@ModelAttribute User user) throws SQLException {
-//        userDAO.updateUser(user);
-//        return "redirect:/My_Profile/userInfo";
-//    }
-//
-//    @GetMapping("/question")
-//    public String question(Model model) {
-//        int userId = 4;
-//        List<Post> posts = postDAO.getPostsByUserId(userId);
-//        model.addAttribute("posts", posts);
-//        return "profile_question";
-//    }
-//
-//    @GetMapping("/post")
-//    public String postIndex(Model model) {
-//        int userId = 4;
-//        List<Post> posts = postDAO.getPostsByUserId(userId);
-//        model.addAttribute("posts", posts);
-//        return "profile_post";
-//    }
-//
-//    @GetMapping("/ChangePassword")
-//    public String changePassword(Model model) throws SQLException {
-//        int userId = 2;
-//        User user = userDAO.getUserByUserId(userId);
-//        model.addAttribute("user", user);
-//        return "profile_change_password";
-//    }
-//
-//    @PostMapping("/ChangePassword")
-//    public String handlePasswordChange(
-//            @RequestParam("password") String oldPassword,
-//            @RequestParam("newPassword") String newPassword,
-//            @RequestParam("confirmPassword") String confirmPassword) throws SQLException {
-//
-//        int userId = 2;
-//        User user = userDAO.getUserByUserId(userId);
-//
-//        if (!user.getPassword().equals(oldPassword)) {
-//            return "Vui lòng nhập mật khẩu khác mật khẩu cũ kkk";
-//        }
-//
-//        if (!newPassword.equals(confirmPassword)) {
-//            return "Mật khẩu không trùng khớp";
-//        }
-//
-//        user.setPassword(newPassword);
-//        userDAO.updateUser(user);
-//
-//        return "redirect:/My_Profile/userInfo?success=Password changed successfully";
-//    }
-//
-//}
-
-//Dùng cái dưới này khi userId được lưu vào session
-
 package com.example.demo.Controller.User;
 import com.example.demo.DAO.NotificationDAO;
 import com.example.demo.DAO.PostDAO;
@@ -106,10 +6,7 @@ import com.example.demo.Model.FileUpload;
 import com.example.demo.Model.Post;
 import com.example.demo.Model.User;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
@@ -118,13 +15,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/My_Profile")
@@ -252,16 +145,23 @@ public class MyProfileController {
         if (file.isEmpty()) {
             return "redirect:/My_Profile/userInfo?error=File is empty";
         }
-        try {
-            String path = "uploads/"+userId+".png";
-            FileCopyUtils.copy(file.getBytes(),new File(path));
+        else{
+            String txt= Objects.requireNonNull(file.getOriginalFilename()).split("\\.")[1];
+            if(txt.equals("png") || txt.equals("jpg")||  txt.equals(".gif")||txt.equals("jpeg") ) {
+                try {
+                    String path = "uploads/" + userId + ".png";
+                    FileCopyUtils.copy(file.getBytes(), new File(path));
 
 //            userDAO.updateAvatar(userId,"/file/"+file.getOriginalFilename());
-        } catch (IOException e)  {
-            e.printStackTrace();
-            return "redirect:/My_Profile/userInfo?error=Could not upload file";
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return "redirect:/My_Profile/userInfo?error=Could not upload file";
+                }
+                return "redirect:/My_Profile/userInfo?success=Avatar changed successfully";
+            }
+            else return "redirect:/My_Profile/userInfo?error=The uploaded file is not a valid image format.";
         }
-        return "redirect:/My_Profile/userInfo?success=Avatar changed successfully";
+
     }
 
 }
